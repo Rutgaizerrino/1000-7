7655807be6174004a9f62601163afc7a9525bcba
Rutgaizerrino <fallout3game@mail.ru>
2021-07-08 15:09:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var button= document.getElementById('button');
+var display= document.getElementById('display');
+
+button.addEventListener('click', kopikaka)
+
+
+
+function kopikaka(){
+    
+    
+    for (let i=1000; i>=0;i= i-7){
+        var kop=i//,+'-7');
+       do {
+        display.textContent=kop;
+       } while (kop>=0) {
+           
+        }
+       
+    // console.log(kop,'-7')
+      
+        }
+    
+}
+
